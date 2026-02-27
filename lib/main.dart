eb44e30e363b6e7312110c18aab2c857ce28add2
dwiilhammaulana <1123150008@global.ac.id>
2026-02-27 21:14:20 +0700

    cardmodel

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => CartModel(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
