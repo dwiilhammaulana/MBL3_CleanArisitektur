ae0bb7da5fa159c6dc1d6c750c0084a73de68a5e
dwiilhammaulana <1123150008@global.ac.id>
2026-03-01 14:48:59 +0700

    menambahkan class myapp

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,33 @@ void main() {
   );
 }
 
+class CartModel extends ChangeNotifier {
+  final List<String> _items = [];
+
+  List<String> get items => _items;
+
+  void add(String itemName) {
+    _items.add(itemName);
+    notifyListeners();
+  }
+
+  void removeAll() {
+    _items.clear();
+    notifyListeners();
+  }
+} 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const MyCatalog(),
+        '/cart': (context) => const MyCart(),
+      },
+    );
   }
 }
