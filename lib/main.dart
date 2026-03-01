20312c30878e6136530e118b7770afd66435b1ad
dwiilhammaulana <1123150008@global.ac.id>
2026-03-01 14:53:59 +0700

    menambahkan class MyCart

--- lib/main.dart
+++ lib/main.dart
@@ -94,3 +94,40 @@ class AddButton extends StatelessWidget {
     );
   }
 }
+
+class MyCart extends StatelessWidget {
+  const MyCart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var cart = context.watch<CartModel>();
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Keranjang Belanja')),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: cart.items.length,
+              itemBuilder: (context, index) =>
+                  ListTile(
+                    leading: const Icon(Icons.fastfood),
+                    title: Text(cart.items[index]),
+                  ),
+            ),
+          ),
+          const Divider(),
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: ElevatedButton(
+              onPressed: () => cart.removeAll(),
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+              child: const Text('Hapus Keranjang',
+                  style: TextStyle(color: Colors.white)),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
